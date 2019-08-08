a2c7906924bc2e732d8ebbbdf2b5fff012d2ef14
hao.zhang <hao.zhang@blowfires.com>
2019-08-08 12:06:25 +0800

    changed: factory pattern note

--- Assets/Scripts/Design Patterns/Factory/Pizza/Pizza/Pizza.cs
+++ Assets/Scripts/Design Patterns/Factory/Pizza/Pizza/Pizza.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
  * @version: 0.0.0
  * @Author: Darcy
  * @Date: 2019-07-31 16:36:48
- * @LastEditTime: 2019-08-07 19:56:37
+ * @LastEditTime: 2019-08-08 12:05:32
  */
 using Ingredient;
 using Libs;
@@ -21,6 +21,13 @@ namespace FactoryPattern
         protected Clam clam;
         protected IPizzaIngredientFactory ingredientFactory;
         
+        /// <summary>
+        /// 由于目前每一种pizza构造方法的不同之处在于原料工厂不同
+        /// 且c#允许抽象类存在构造方法
+        /// 所以这里在抽象类中构建构造方法
+        /// 子类继承父类时只需继承构造方法传入原料工厂引用即可
+        /// </summary>
+        /// <param name="ingredientFactory"></param>
         protected Pizza(IPizzaIngredientFactory ingredientFactory){
             this.ingredientFactory = ingredientFactory;
         }

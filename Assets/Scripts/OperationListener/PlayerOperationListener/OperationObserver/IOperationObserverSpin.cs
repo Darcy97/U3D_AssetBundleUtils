35892991a251e02f21e850a6ec41c34d944652b3
hao.zhang <hao.zhang@blowfires.com>
2019-08-19 20:34:39 +0800

    fix

--- Assets/Scripts/OperationListener/PlayerOperationListener/OperationObserver/IOperationObserverSpin.cs
+++ Assets/Scripts/OperationListener/PlayerOperationListener/OperationObserver/IOperationObserverSpin.cs
@@ -3,8 +3,9 @@
  * @version: 0.0.0
  * @Author: Darcy
  * @Date: 2019-08-19 17:27:39
- * @LastEditTime: 2019-08-19 20:25:02
+ * @LastEditTime: 2019-08-19 20:29:26
  */
+
 
 using System.Runtime.CompilerServices;
 namespace OperationListener
